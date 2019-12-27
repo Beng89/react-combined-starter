b99ba9e0d47d0d7136eb1dcb0bea8ac4ae79d1dc
Ben Goltz <ben.goltz@outlook.com>
2019-12-27 17:55:49 -0600

    Updated versions of several packages and fixed configs.

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -15,14 +15,14 @@ module.exports = {
   // target: 'node',
   resolve: {
     // Add '.ts' and '.tsx' as resolvable extensions.
-    extensions: [".ts", ".tsx", ".js", ".json"]
+    extensions: [".ts", ".tsx", ".js", ".json", ".css"]
   },
 
   module: {
     rules: [
       // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
       { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
-
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
       // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
       { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
 
